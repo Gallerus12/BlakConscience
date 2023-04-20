e59aa4d6fa9ebbdf01a0bf469754c74191b227cf
Gallerus12 <38595131+Gallerus12@users.noreply.github.com>
2023-04-20 17:07:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const cloudinary = require('cloudinary').v2;
 
 
 
-app.use(cors());
+app.use(cors({
+  credentials: true, 
+  origin: 'https://www.blakconscience.com'}));
 app.use(express.json());
 //app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(bodyParser.json())
